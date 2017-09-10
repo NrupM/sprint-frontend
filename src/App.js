40191a67c592a47474a751a57591f590973e6249
NrupM <maryenorthrup@gmail.com>
2017-09-09 23:31:23 -0700

    render gmap

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<div className="App-header">
-					<div id="map" />
-				</div>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
+				<Map
+					google={window.google}
+					className={'map'}
+					zoom={4}
+					style={{ width: '100%', height: '100%', position: 'absolute' }}
+				/>
 			</div>
 		);
 	}
--- src/components/container.js
+++ src/components/container.js
@@ -1,15 +1,1 @@
-import React, { Component } from 'react';
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
-
-export class Container extends Component {
-	render() {
-		if (!this.props.loaded) {
-			return <div>Loading...</div>;
-		}
-		return <div>Map will go here</div>;
-	}
-}
-
-export default GoogleApiComponent({
-	apiKey: AIzaSyCnyu2FJac70 - X0EXKaoIxVw5RB4luN0uk
-})(MapContainer);
+// imp
